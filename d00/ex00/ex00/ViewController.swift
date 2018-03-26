ba6d4759c9e8ed0c9b3cfcca5f9f6248c05e973a
Adubedat <arthur.dubedat@gmail.com>
2018-03-26 14:34:34 +0200

    ex00

---
+++ d00/ex00/ex00/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Arthur DUBEDAT on 3/26/18.
+//  Copyright © 2018 Arthur DUBEDAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func debugButton(_ sender: Any) {
+        print("Hello world !")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
