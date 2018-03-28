f051bb8b7bddbfc7ad7b6f65a6ddd5c3758d7c8c
Adubedat <arthur.dubedat@gmail.com>
2018-03-28 15:05:52 +0200

    ex02

---
+++ D01/ex02/main.swift
@@ -0,0 +1,33 @@
+let spades = Deck.allSpades
+let hearts = Deck.allHearts
+let diamonds = Deck.allDiamonds
+let clubs = Deck.allClubs
+let deck = Deck.allCards
+
+for elem in spades {
+	print(elem)
+}
+
+print("\n/**************************************\\\n")
+
+for elem in hearts {
+	print(elem)
+}
+
+print("\n/**************************************\\\n")
+
+for elem in diamonds {
+	print(elem)
+}
+
+print("\n/**************************************\\\n")
+
+for elem in clubs {
+	print(elem)
+}
+
+print("\n/**************************************\\\n")
+
+for elem in deck {
+	print(elem)
+}
